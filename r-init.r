8e2becce28a6c3f2e2a1fac1d29e8a26ef346f0b
Otak <kalab.oto@gmail.com>
2018-10-14 15:02:35 +0200

    add RSToolbox package

--- r-init.r
+++ r-init.r
@@ -1,3 +1,3 @@
-install.packages(c('curl', 'repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest'))
+install.packages(c('curl', 'repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest','RStoolbox'))
 devtools::install_github('IRkernel/IRkernel')
 IRkernel::installspec(user = FALSE)
