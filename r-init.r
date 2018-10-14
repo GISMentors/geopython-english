02ba32a1f4042a4f74c6ecdbf4b3bc4a75f32aff
Otak <kalab.oto@gmail.com>
2018-10-14 23:41:06 +0200

    add gdalUtils

--- r-init.r
+++ r-init.r
@@ -1,3 +1,3 @@
-install.packages(c('curl', 'repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest','RStoolbox'))
+install.packages(c('curl', 'repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest','RStoolbox','gdalUtils'))
 devtools::install_github('IRkernel/IRkernel')
 IRkernel::installspec(user = FALSE)
